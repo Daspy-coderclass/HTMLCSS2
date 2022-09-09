9d9fa9c077d5c540206ea1e404a2d9b5d1206804
David Spits <david.spits@coderclass.nl>
2022-09-09 11:55:16 +0200

    CSS gedaan en een sidebar gemaakt en waarschijnlijk meer

--- badges.js
+++ badges.js
@@ -1,8 +1,6 @@
 var list = document.getElementsByTagName("li");
-console.log(list);
+
 for (let i = 1; i < list.length+1; i++) {
   var a = `url("badgeimages/`+i+`.png")`
-  console.log(a);
   list[i-1].style.backgroundImage = a;
-  console.log(list[i]);
 }
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const projectList =["cure.inc","rekenmachine","template1","template2","template3"];
+const projectList =["Cure.inc","Gamejam: Multiversum","First Lego league"];
 var UI_project = document.getElementById("hier_project");
 UI_project.innerHTML = projectList[Math.floor(Math.random()*projectList.length)];
+
+var hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function(){
+      document.querySelector("body").classList.toggle("active");
+  })
