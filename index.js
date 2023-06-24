96cf86187b0a57c98dc1a37a45f1052a85843e62
Daspy-coderclass <david.spits@coderclass.nl>
2023-06-24 21:23:06 +0200

    Project afgerond

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 if (document.getElementById("hier_project") != null) {
-  const projectList =["Cure.inc","Gamejam: Multiversum","First Lego league"];
+  const projectList =[["Cure.inc","Images/cureinc.png"],["Gamejam: Multiversum","Images/multi_logo.png"],["The Brightbox","kaas"],];
+  var randomProject = Math.floor(Math.random()*projectList.length);
+  console.log(projectList[randomProject][0]);
   var UI_project = document.getElementById("hier_project");
-  UI_project.innerHTML = projectList[Math.floor(Math.random()*projectList.length)];
+  UI_project.innerHTML = projectList[randomProject][0];
+  var IMG_project = document.getElementById("foto_project");
+  IMG_project.outerHTML = '<img id="foto_project" src="'+projectList[randomProject][1]+'">';
 }
-
-var hamburger = document.querySelector(".hamburger");
-  hamburger.addEventListener("click", function(){
-      document.querySelector("body").classList.toggle("active");
-  })
